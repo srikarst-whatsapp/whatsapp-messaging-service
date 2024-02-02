b4e5896e2d8ad0f2e4e835e4a829f4e4a58a30a9
Srikar charan <srikarsuri@gmail.com>
2024-02-02 16:07:01 +0530

    Project init

---
+++ src/main/java/com/whatsapp/whatsappmessagingservice/WhatsappMessagingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.whatsapp.whatsappmessagingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhatsappMessagingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhatsappMessagingServiceApplication.class, args);
+	}
+
+}
