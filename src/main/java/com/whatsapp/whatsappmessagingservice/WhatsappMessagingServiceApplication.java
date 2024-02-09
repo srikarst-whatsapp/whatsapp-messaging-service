e4c9e3c3ea885dfd824710447e73559b472ff5d3
srikarst <srikarsuri@gmail.com>
2024-02-09 17:45:22 +0000

    Rename Application class

--- src/main/java/com/whatsapp/whatsappmessagingservice/WhatsappMessagingServiceApplication.java
+++ src/main/java/com/whatsapp/whatsappmessagingservice/WhatsappMessagingServiceApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class WhatsappAuthServiceApplication {
+public class WhatsappMessagingServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(WhatsappAuthServiceApplication.class, args);
+		SpringApplication.run(WhatsappMessagingServiceApplication.class, args);
 	}
 
 }
