d8347979146bd7a6fc299fea1c7e75281905be4c
srikarst <srikarsuri@gmail.com>
2024-02-13 08:38:09 +0000

    Add BCryptPasswordEncoder bean

--- src/main/java/com/whatsapp/whatsappmessagingservice/WhatsappMessagingServiceApplication.java
+++ src/main/java/com/whatsapp/whatsappmessagingservice/WhatsappMessagingServiceApplication.java
@@ -2,12 +2,19 @@ package com.whatsapp.whatsappmessagingservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class WhatsappMessagingServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WhatsappMessagingServiceApplication.class, args);
+	}
+
+	@Bean
+	BCryptPasswordEncoder bCryptPasswordEncoder() {
+		return new BCryptPasswordEncoder();
 	}
 
 }
